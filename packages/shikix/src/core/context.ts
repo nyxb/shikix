32eb5bc7f4cdc28bcad5ea1bbc9ec695825766a1
nyxb <contact@nyxb.email>
2023-11-29 12:10:08 +0100

    ✨ feat(core): add core functionality for shikix package
    - Implement bundle factory for creating highlighters with bundled themes and languages.
    - Provide shiki context for minimal rendering setup.
    - Create core highlighter logic with support for HTML, Hast, and token rendering.
    - Add utility functions and normalization for theme handling.

---
+++ packages/shikix/src/core/context.ts
@@ -0,0 +1,107 @@
+import type { HighlighterCoreOptions, LanguageInput, MaybeGetter, ShikiContext, ThemeInput, ThemeRegistration } from '../types'
+import { createOnigScanner, createOnigString, loadWasm } from '../oniguruma'
+import { Registry } from './registry'
+import { Resolver } from './resolver'
+
+/**
+ * Get the minimal shiki context for rendering.
+ */
+export async function getShikiContext(options: HighlighterCoreOptions = {}): Promise<ShikiContext> {
+   async function normalizeGetter<T>(p: MaybeGetter<T>): Promise<T> {
+      return Promise.resolve(typeof p === 'function' ? (p as any)() : p).then(r => r.default || r)
+   }
+
+   async function resolveLangs(langs: LanguageInput[]) {
+      return Array.from(new Set((await Promise.all(
+         langs.map(async lang => await normalizeGetter(lang).then(r => Array.isArray(r) ? r : [r])),
+      )).flat()))
+   }
+
+   const [
+      themes,
+      langs,
+   ] = await Promise.all([
+      Promise.all((options.themes || []).map(normalizeGetter)),
+      resolveLangs(options.langs || []),
+      typeof options.loadWasm === 'function'
+         ? Promise.resolve(options.loadWasm()).then(r => loadWasm(r))
+         : options.loadWasm
+            ? loadWasm(options.loadWasm)
+            : undefined,
+   ] as const)
+
+   const resolver = new Resolver(Promise.resolve({
+      createOnigScanner(patterns) {
+         return createOnigScanner(patterns)
+      },
+      createOnigString(s) {
+         return createOnigString(s)
+      },
+   }), 'vscode-oniguruma', langs)
+
+   const _registry = new Registry(resolver, themes, langs)
+   Object.assign(_registry.alias, options.langAlias)
+   await _registry.init()
+
+   function getLangGrammar(name: string) {
+      const _lang = _registry.getGrammar(name)
+      if (!_lang)
+         throw new Error(`[shikix] Language \`${name}\` not found, you may need to load it first`)
+      return _lang
+   }
+
+   function getTheme(name: string | ThemeRegistration) {
+      const _theme = _registry.getTheme(name!)
+      if (!_theme)
+         throw new Error(`[shikix] Theme \`${name}\` not found, you may need to load it first`)
+      return _theme
+   }
+
+   function setTheme(name: string | ThemeRegistration) {
+      const theme = getTheme(name)
+      _registry.setTheme(theme)
+      const colorMap = _registry.getColorMap()
+      return {
+         theme,
+         colorMap,
+      }
+   }
+
+   function getLoadedThemes() {
+      return _registry.getLoadedThemes()
+   }
+
+   function getLoadedLanguages() {
+      return _registry.getLoadedLanguages()
+   }
+
+   async function loadLanguage(...langs: LanguageInput[]) {
+      await _registry.loadLanguages(await resolveLangs(langs))
+   }
+
+   async function loadTheme(...themes: ThemeInput[]) {
+      await Promise.all(
+         themes.map(async theme => _registry.loadTheme(await normalizeGetter(theme))),
+      )
+   }
+
+   function updateAlias(alias: Record<string, string>) {
+      Object.assign(_registry.alias, alias)
+   }
+
+   function getAlias() {
+      return _registry.alias
+   }
+
+   return {
+      setTheme,
+      getTheme,
+      getLangGrammar,
+      getLoadedThemes,
+      getLoadedLanguages,
+      getAlias,
+      updateAlias,
+      loadLanguage,
+      loadTheme,
+   }
+}
---
+++ packages/shikix/src/core/index.ts
@@ -0,0 +1,10 @@
+export { loadWasm } from '../oniguruma'
+
+export * from './context'
+export * from './highlighter'
+export * from './bundle-factory'
+export * from './utils'
+
+export { codeToThemedTokens } from './tokenizer'
+export { tokenizeAnsiWithTheme } from './tokenizer-ansi'
+export { toShikiTheme } from './normalize'
